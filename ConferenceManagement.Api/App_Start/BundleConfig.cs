c2a5a6ae36b584b2f130ce9b2d3abef719e547f8
satish <satish860@gmail.com>
2012-11-30 10:28:30 +0530

    Inital Checkin for the weekend hackathon :)
    Conference management is SPA based application with Continous Delivery
    principle.

---
+++ ConferenceManagement.Api/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ConferenceManagement.Api
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/backbone").Include(
+                       "~/Scripts/underscore.js", "~/Scripts/backbone.js", "~/Scripts/handlebar.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+           
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css","~/Content/bootstrap-responsive.css","~/Content/site.css"));
+
+        }
+    }
+}
---
+++ ConferenceManagement.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConferenceManagement.Api.MvcApplication" Language="C#" %>
